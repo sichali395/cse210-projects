6d942906381f6b49c850b9f87aef1af697b43e2a
sichali395 <lsichali@byupathway.edu>
2025-09-14 20:35:34 +0200

    add program css

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,28 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create job instances
         Job job1 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
-        job1._startYear = 2019;
-        job1._endYear = 2022;
-        
+        job1._startYear = 2015;
+        job1._endYear = 2019;
+
         Job job2 = new Job();
-        job2._jobTitle = "Data Scientist";
-        job2._company = "Google";
-        job2._startYear = 2022;
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2020;
         job2._endYear = 2024;
-        
-        // Create resume instance with your name
+
         Resume myResume = new Resume();
         myResume._name = "Laston James Sichali";
-        
-        // Add jobs to the resume
+
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
-        
-        // Display the complete resume
+
         myResume.Display();
     }
 }
